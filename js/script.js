ea083835aaec467961e5d9348acb1067048ed8b2
Giuseppe Steduto <peppesteduto@gmail.com>
2022-11-07 17:39:17 -0600

    Added JS code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 function init() {
 //add your javascrip between these two lines of code
+    function updateText() {
+        var userInput = document.getElementById("entryinput").value;
 
+        document.getElementById("textoutput").innerText = userInput;
+        alert("Giuseppe Steduto: " + userInput);
+    }
 
+    var button = document.getElementById("entrybutton");
+    button.addEventListener("click", updateText);
 }
 window.addEventListener('load', init);
